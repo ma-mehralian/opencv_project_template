8c8148564688e547b35af244d6ea18cce6ae3131
ma.mehralian <ma.mehralian@gmail.com>
2022-08-12 21:28:03 +0430

    code cleaned

--- modules/roi_grabber/examples/main.cpp
+++ modules/roi_grabber/examples/main.cpp
@@ -6,19 +6,19 @@ using namespace std;
 using namespace cv;
 
 /*!
- * This is a very simple test for roi_grabber module!
+ * This is a very simple example for roi_grabber module!
  */
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[]){
 	Mat img = imread("xyz.png");
-    if(img.empty()){
-        cout << "Cannot read image!" << endl;
-        return -1;
-    }
+	if (img.empty()){
+		cout << "Cannot read image!" << endl;
+		return -1;
+	}
 
 	ROIGrabber gr;
 	Rect r = gr.GetRoi(img);
-    //fill it with white color
-	img(r).setTo(Scalar(255,255,255));
+	// fill it with white color
+	img(r).setTo(Scalar(255, 255, 255));
 	imshow("output", img);
 	waitKey();
 	cout << "Test Finished Succefully!" << endl;
